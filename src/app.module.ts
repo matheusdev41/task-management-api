506fd674d811bff361c6b5dd1beaafcda8117fef
Matheus Pessoa <matheus41.dev@gmail.com>
2026-01-21 00:54:11 -0300

    feat(auth): configurar lógica de autenticação e login

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 import { PostsModule } from './posts/posts.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -19,6 +21,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     }),
     TasksModule,
     PostsModule,
+    UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
